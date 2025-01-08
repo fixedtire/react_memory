d20025783b3695b6671993c01e4bd7354aca09da
bencake <hagstrom_b@gmx.de>
2025-01-08 22:59:49 +0100

    add flip logic with usestate and useeffect

--- src/MemoryCard.tsx
+++ src/MemoryCard.tsx
@@ -1,4 +1,5 @@
 import './App.scss'
+import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
@@ -7,20 +8,53 @@ interface MemoryCardProps {
 }
 
 const MemoryCard: React.FC<MemoryCardProps> = ({title}) => {
-   
+
+  // State of one memory card, if false, memory card is hidden
+
+  const [cardHidden, turnCard] = useState(true);
+
+  // toggles the state of this memory card and flips it
+
+  const flipCard = () => {
+    turnCard(!cardHidden);
+  };
+
+  useEffect(() => {
+    const cardElement = document.getElementById('card');
+    if (cardElement) {
+      cardHidden
+        ? cardElement.classList.add('flipped')
+        : cardElement.classList.remove('flipped');
+    }
+  }, [cardHidden]);
+
+        
     return (
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>{title}</Card.Title>
-              <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        );
+      <div className="flip-card" id="card" onClick={flipCard}>
+
+  <div className="flip-card-inner">
+
+    <div className="flip-card-front">
+
+      <h1>{title}</h1>
+
+    </div>
+
+    <div className="flip-card-back">
+
+      <h1>Back</h1>
+
+    </div>
+
+  </div>
+
+</div>
+    )
+            
+              {/* Bootstrap Style Card for one Memory Card
+
+              
+              */}
 }
 
 export default MemoryCard
